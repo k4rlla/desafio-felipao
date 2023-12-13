46430721545d099c8910d92a153a5f0966003eb8
k4rlla <karllinha.sousa@gmail.com>
2023-12-13 16:38:23 -0300

    Nível de Herói

---
+++ src/desafio.js
@@ -0,0 +1,20 @@
+let nome = "Din Djarin";
+let xp = 1000;
+
+if (xp < 1000) {
+    console.log("Ferro")
+} else if(xp >= 1001 && xp <= 2000) {
+    console.log("Bronze")
+} else if(xp >= 2001 && xp <= 5000) {
+    console.log("Prata")
+} else if(xp >= 6001 && xp <= 7000) {
+    console.log("Ouro")
+} else if(xp >= 7001 && xp <= 8000) {
+    console.log("Platina Diamante")
+} else if(xp >= 8001 && xp <= 9000) {
+    console.log("Ascendente")
+} else if(xp >= 9001 && xp<= 10000) {
+    console.log("Imortal")
+} else {
+    console.log("Radiante")
+}
